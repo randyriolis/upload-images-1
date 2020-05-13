757de2b09efa933a2bcc1331286d13097810dd0c
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2020-05-14 00:01:17 +0700

    feat: add laravel auth

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
 const mix = require('laravel-mix');
+const js = 'public/js/';
+const css = 'public/css/';
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +13,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+if (mix.inProduction()) {
+    mix.version();
+}
+
+mix.js('resources/js/app.js', js)
+    .sass('resources/sass/app.scss', css);
