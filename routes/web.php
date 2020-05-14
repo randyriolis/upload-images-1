46f302ee5e054a9572dba9a3248e5d2d1e365041
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2020-05-15 04:53:26 +0700

    fix: route closure saat jalankan artisan optimize

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
 Auth::routes();
 
 Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.', 'middleware' => ['auth']], function () {
-    Route::get('/', function () {
-        return redirect()->route('dashboard.categories.index');
-    });
+    Route::redirect('/', 'dashboard/categories');
 
     Route::resource('categories', 'CategoryController');
 });
